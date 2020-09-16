183d014bf96b46a630c92c12c7b93394ff0e4869
TheAshtonaught <ashtomorgan3@gmail.com>
2020-09-15 22:03:55 -0500

    weekday collection view size established

--- Appointment_Booker/FirstViewController.swift
+++ Appointment_Booker/FirstViewController.swift
@@ -10,24 +10,38 @@ import UIKit
 
 class FirstViewController: UIViewController {
     let reuseIdentifier = "weekdayCell"
-    
-    
+    //let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    @IBOutlet weak var weekdayCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+    }
 
 }
 
+// MARK: UICollectionViewDataSource
 extension FirstViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 7
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
         
         return cell
+    }
+}
+
+// MARK: UICollectionViewFlowLayoutDelegate
+extension FirstViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = view.frame.width / 3.5
+        
+        return CGSize(width: cellWidth, height: collectionView.frame.height)
     }
 }
